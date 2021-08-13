21b0003178d756382db01a39489ec6e4b58e0166
Behzad Amirinezhad <behzad.amiri.nezhad@gmail.com>
2021-08-14 03:07:18 +0430

    automated publish / test2

--- publish.sh
+++ publish.sh
@@ -7,11 +7,9 @@ read commitMessage
 
 git commit -m "$commitMessage"
 
-echo 'Enter the name of the branch:'
-read branch
+# echo 'Enter the name of the branch:'
+# read branch
 
-git push origin $branch
-
-y
+git push origin main
 
 read
