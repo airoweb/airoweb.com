ea82a972877f7ea12f04eb7fe1aa20d36e7f2a1f
Behzad <behzad.amiri.nezhad@outlook>
2021-08-15 15:29:18 +0430

    add windows publishwin.sh

---
+++ publishwin.sh
@@ -0,0 +1,15 @@
+../zola.exe build
+
+git add .
+
+echo 'Enter the commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+# echo 'Enter the name of the branch:'
+# read branch
+
+git push origin main
+
+exit
